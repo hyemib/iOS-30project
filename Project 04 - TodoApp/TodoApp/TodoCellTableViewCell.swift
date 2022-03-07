61bba3e9305a74db60cc7ee0cf30a1e01e91bf13
Hhyemm <hyem0127@gmail.com>
2022-03-07 23:46:25 +0900

    Add : Project 04 - TodoApp

---
+++ Project 04 - TodoApp/TodoApp/TodoCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TodoCellTableViewCell.swift
+//  TodoApp
+//
+//  Created by hyemi on 2022/03/07.
+//
+
+import UIKit
+
+class TodoCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
